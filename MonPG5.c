8945d24e2a75603862f84e83fac32d36e31c0d0c
unknown <benjamincligny@gmail.com>
2019-03-22 16:55:02 +0100

    programme numéro 5

---
+++ MonPG5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    string mot;
+
+    printf("Bienvenue dans le programme de la politesse !\n");
+
+    printf("Ecrivez bonjour, le terminal vous répondra: ");
+    if(scanf("%d\n", &mot) == "bonjour" || "Bonjour"){
+        printf("Bonjour !!!")
+    }
+}
+

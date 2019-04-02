df32445ef727864ead81ad717fc531fe0bcdc792
North Justin <jnorth@u18-bionic-0828.univ-lr.fr>
2019-04-02 17:37:07 +0200

    Media player done

--- src/MediaPlayer.c
+++ src/MediaPlayer.c
@@ -1,31 +1,8 @@
 #include <stdlib.h>
-#include <gtk-3.0/gtk/gtk.h>
 
 int main(int argc, char const *argv[])
 {
-    GtkWidget *p_Window;
-    GtkWidget *p_Label;
-    gchar *sUtf8;
-
-    gtk_init(&argc, &argv);
-
-    p_Window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(p_Window), "GTK+ 2.10");
-    gtk_window_set_default_size(GTK_WINDOW(p_Window), 260, 40);
-    gtk_window_set_position(GTK_WINDOW(p_Window), GTK_WIN_POS_CENTER);
-    g_signal_connect(G_OBJECT(p_Window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
-
-    sUtf8 = g_locale_to_utf8("La Bibliothèque GTK+ à bien été Installée !", -1, NULL, NULL, NULL);
-    p_Label = gtk_label_new(sUtf8);
-    g_free(sUtf8);
-    gtk_container_add(GTK_CONTAINER(p_Window), p_Label);
-
-    gtk_widget_show_all(p_Window);
-
-    gtk_main();
+    system("xdg-open ../img/img1.jpg");
 
     return EXIT_SUCCESS;
 }
-
-//export PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:/usr/local/opt/libffi/lib/pkconfig"
-//gcc $(pkg-config --libs --cflags gtk+-3.0) MediaPlayer.c -o MediaPlayer

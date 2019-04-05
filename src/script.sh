0966652849ac9794cbf9f913c020e8dc23f424f6
Justin North <justin_n@hotmail.fr>
2019-04-05 10:24:46 +0200

    Ajout d'un script pour générer les .exe

---
+++ src/script.sh
@@ -0,0 +1,6 @@
+gcc -Wall MonPG1.c -o MonPG1.exe
+gcc -Wall MonPG2.c -o MonPG2.exe
+gcc -Wall MonPG3.c -o MonPG3.exe
+gcc -Wall MonPG4.c -o MonPG4.exe
+gcc -Wall MonPG5.c -o MonPG5.exe
+gcc -Wall MediaPlayer.c -o MediaPlayer.exe

